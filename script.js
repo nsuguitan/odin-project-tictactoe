6aa64afb3a22ea35cb0ce4c78dae4e987a8f18dd
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-03-20 01:16:55 -0400

    link winner check logic to game

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
 const Player = (name,symbol) => { //factory function for tic tac toe players
     const getName = () => name;
     const getSymbol = () => symbol;
-    const takeTurn = () => {
-        position = 1; //get val from grid
-        return position;
-
-    };
     const winner = () => {
 
     };
-    return{getName, getSymbol, takeTurn, winner}
+    return{getName, getSymbol, winner}
 }
 
-function checkWinner(lastMove, board){
-//check row
-rowStart = Math.floor(lastMove/3) * 3
-console.log("Indexes checked for Row = ", rowStart, rowStart+1, rowStart + 2)
-if(Math.abs(board[rowStart] + board[rowStart + 1] + board[rowStart+2]) == 3){return True}
-//check col
-console.log("Indexes checked for Col = ", lastMove, (lastMove + 3)%9, (lastMove + 6)%9)
-if(Math.abs(board[lastMove] + board[(lastMove+3)%9] + board[(lastMove + 6)%9]) == 3){return True}
-//check left diagonal
-if(lastMove%4 == 0 && Math.abs(board[0] + board[4] + board[8]) == 3){return True}
-//check right diagonal, but checks when position 0 and 8 are played as well
-if(lastMove%2 == 0 && Math.abs(board[2] + board[4] + board[6]) == 3){return True}
-} ;
+
 
 
 const gameBoard = (() => {
     board = Array(9).fill("0")
-    turnCount = 0;
-    const update = (position) => {
-        if(turnCount % 2 == 0) {board[position] = 1}
-        else{board[position] = -1};
-        turnCount ++;       
-    }    
+    const update = (buttonId) => {
+        position = buttonId.charAt(1)
+        if(play.getTurn().getSymbol() == "X") {board[position] = 1}
+        else{board[position] = -1};  
+    } 
+    const getBoard =() =>{
+        return board;
+    };  
+    return{update, getBoard}
 }
 )();
 
@@ -45,9 +31,15 @@ const play = (() => {
     const takeTurn = (buttonId) =>{
         if (validMoveCheck(buttonId)){
             displayController.updateSquare(buttonId);
+            gameBoard.update(buttonId);
+            setTimeout(() => {
+                if(checkWinner(buttonId)){
+                    alert("Winner winner chicken dinner!")
+                };
+                }, 200);
             changeTurn();
         }
-    }
+    };
     const validMoveCheck = (buttonId) => {
         var elem = document.getElementById(buttonId);
         if (elem.textContent=="") {
@@ -71,7 +63,24 @@ const play = (() => {
         console.log("This is p1:",p1);
         return turn;
     };
-    return{changeTurn, getTurn, takeTurn, validMoveCheck}
+    const checkWinner = (buttonId) => {
+        lastMove = buttonId.charAt(1);
+        board = gameBoard.getBoard();
+        //check row
+        rowStart = Math.floor(lastMove/3) * 3
+        console.log("Indexes checked for Row = ", rowStart, rowStart+1, rowStart + 2)
+        if(Math.abs(board[rowStart] + board[rowStart + 1] + board[rowStart+2]) == 3){return true}
+        //check col
+        console.log("Indexes checked for Col = ", lastMove, (lastMove + 3)%9, (lastMove + 6)%9)
+        if(Math.abs(board[lastMove] + board[(lastMove+3)%9] + board[(lastMove + 6)%9]) == 3){return true}
+        //check left diagonal
+        if(lastMove%4 == 0 && Math.abs(board[0] + board[4] + board[8]) == 3){return true}
+        //check right diagonal, but checks when position 0 and 8 are played as well
+        if(lastMove%2 == 0 && Math.abs(board[2] + board[4] + board[6]) == 3){return true}
+        else {return false} 
+        }
+        ;
+    return{changeTurn, getTurn, takeTurn, validMoveCheck, checkWinner}
 }
 )(); 
 const displayController = (() => {
